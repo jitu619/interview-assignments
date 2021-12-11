0bbdbc75cb6c89bd67dcfd5fb429f864dc80b6ce
jitu619 <63717345+jitu619@users.noreply.github.com>
2021-12-11 16:55:50 +0530

    Update closest-subsequence-sum.cpp

--- Array/closest-subsequence-sum.cpp
+++ Array/closest-subsequence-sum.cpp
@@ -4,7 +4,6 @@ public:
     vector<int> powerset(vector<int> &nums,int start,int end){
         vector<int> v;
         int n=end-start+1;
-        // cout<<start<<" "<<end<<"\n";
         for(int msk=0;msk<(1<<n);msk++){
             int ans=0;
             for(int i=0;i<n;i++){
@@ -20,64 +19,41 @@ public:
     int lowerBound(vector<int> &nums,int target){
         int low=0,high=nums.size()-1;
         int ans=0;
+        bool flag=false;
         while(low<=high){
             int mid=low+(high-low)/2;
-            // cout<<low<<" "<<mid<<" "<<high<<" "<<target<<"\n";
             if(nums[mid]==target){
-                return nums[mid];
-            }
-            if(nums[mid]<target){
-                ans=nums[mid];
-                low=mid+1;
-            }else{
-                high=mid-1;
-            }
-        }
-        return ans;
-    }
-    
-    int upperBound(vector<int> &nums,int target){
-        int low=0,high=nums.size()-1;
-        int ans=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            // cout<<low<<" "<<mid<<" "<<high<<" "<<target<<"\n";
-            if(nums[mid]==target){
-                return nums[mid];
+                return mid;
             }
             if(nums[mid]<target){
                 low=mid+1;
             }else{
-                ans=nums[mid];
+                ans=mid;
+                flag=true;
                 high=mid-1;
             }
         }
-        return ans;
+        return flag?ans:nums.size();
     }
     
     int minAbsDifference(vector<int>& nums, int goal) {
         int n=nums.size();
         vector<int> firstHalf=powerset(nums,0,n/2);
         vector<int> secondHalf=powerset(nums,n/2+1,n-1);
-        // for(int i:firstHalf){
-        //     cout<<i<<" ";
-        // }
-        // cout<<"\n";
-        // for(int i:secondHalf){
-        //     cout<<i<<" ";
-        // }
-        // cout<<"\n";
+      
         int answer=INT_MAX;
         sort(secondHalf.begin(),secondHalf.end());
         for(int i=0;i<firstHalf.size();i++){
-            // cout<<"i: "<<i<<"\n";
             int target=goal-firstHalf[i];
-            int lb=lowerBound(secondHalf,target);
-            int ub=upperBound(secondHalf,target);
-            answer=min(answer,min(abs(target-lb),abs(target-ub)));
+            int lbIdx=lowerBound(secondHalf,target);
+            // Comparing both j-1 and jth index to find out minium difference.
+            if(lbIdx>0){
+                answer=min(answer,abs(target-secondHalf[lbIdx-1]));
+            }
+            if(lbIdx<secondHalf.size()){
+                answer=min(answer,abs(target-secondHalf[lbIdx]));
+            }
         }
         return answer;
     }
 };
-
-// 5596358 5531914
